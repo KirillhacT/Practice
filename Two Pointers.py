37098b623a6dec283f1638f48a55c64e8cd11d2e
KirillhacT <gyynhild@gmail.com>
2022-12-11 12:52:51 +0300

    two_pointers

--- Two Pointers.py
+++ Two Pointers.py
@@ -4,4 +4,35 @@ def Squares_of_a_Sorted_Array(nums):
     return sorted(nums)
 
 nums = [-4, -1, 0, 3, 10]
-print(Squares_of_a_Sorted_Array(nums))
+# print(Squares_of_a_Sorted_Array(nums))
+
+def rotate_array(nums, k):
+    cou = len(nums)-k
+    num1 = nums[:cou]
+    num2 = nums[cou:]
+    return num2 + num1
+
+numsik = [-1,-100,3,99]
+print(rotate_array(numsik, 2))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
